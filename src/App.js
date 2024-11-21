e7ce8e898b6b7682ae88c347ac13bbe510d2a571
atukoon <atsuko.o.d22@gmail.com>
2024-11-21 18:45:18 +0900

    feat: 基本UI構築、soptifyAPI連携、音楽再生停止処理実装

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import spotify from './lib/spotify';
+import SongList from './components/SongList';
+import Player from './components/Player';
+import { useEffect, useRef, useState } from 'react';
 
-function App() {
+export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [poplarSongs, setPoplarSongs] = useState([]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [selectedSong, setSelectedSong] = useState();
+  const audioRef = useRef();
+
+  useEffect(() => {
+    fetchPoplarSongs();
+  }, []);
+
+  const fetchPoplarSongs = async () => {
+    setIsLoading(true);
+    const result = await spotify.getPopularSongs();
+    const songs = result.items.map((item) => {
+      return item.track;
+    });
+    setPoplarSongs(songs);
+    setIsLoading(false);
+  };
+
+  const handleSelectedSong = async (song) => {
+    setSelectedSong(song);
+    if (song.preview_url) {
+      audioRef.current.src = song.preview_url;
+      playSong();
+    }else {
+      pauseSong();
+    }
+  };
+
+  const playSong = () => {
+    audioRef.current.play();
+    setIsPlaying(true);
+  };
+
+  const pauseSong = () => {
+    audioRef.current.pause();
+    setIsPlaying(false);
+  };
+
+  const togglePlay = () => {
+    if (isPlaying) {
+      pauseSong();
+    } else {
+      playSong();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+      <main className="flex-1 p-8 mb-20">
+        <header className="flex justify-between items-center mb-10">
+          <h1 className="text-4xl font-bold">Music App</h1>
+        </header>
+        <section>
+          <h2 className="text-2xl font-semibold mb-5">Popular Songs</h2>
+          <SongList
+            isLoading={isLoading}
+            songs={poplarSongs}
+            onSelectedSong={handleSelectedSong}
+          />
+        </section>
+      </main>
+      {selectedSong != null && (
+        <Player song={selectedSong} isPlaying={isPlaying} onButtonClick={togglePlay} />
+      )}
+      <audio ref={audioRef} />
     </div>
   );
 }
-
-export default App;
---
+++ src/lib/spotify.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+class SpotifyClient {
+  static async initialize() {
+    const result = await axios.post(
+      'https://accounts.spotify.com/api/token',
+      {
+        grant_type: 'client_credentials',
+        client_id: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
+        client_secret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET,
+      },
+      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+    );
+
+    let spotify = new SpotifyClient();
+    spotify.token = result.data.access_token;
+    return spotify;
+  }
+
+  async getPopularSongs() {
+    const response = await axios.get(
+      'https://api.spotify.com/v1/playlists/37i9dQZF1DX9vYRBO9gjDe/tracks',
+      {
+        headers: {
+          Authorization: 'Bearer ' + this.token,
+        },
+      }
+    );
+    return response.data;
+  }
+}
+
+const spotify = await SpotifyClient.initialize();
+export default spotify;
